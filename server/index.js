ccbe18b0e252e0d1da27622c2cad27096efc687f
Cody Jewell <cjewell468@gmail.com>
2017-03-16 23:58:16 -0400

    Connect to mLab database
    Using MONGOLAB_URI as the variable to store the mLab
    connection as a process.env variable inside of a .env
    file that git is not tracking.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 
 const port = process.env.PORT || 3000;
 const app = express();
 const env = process.env.NODE_ENV || 'development';
 
 
+dotenv.config();
+
+
 if (env === 'development') {
   app.use(morgan('dev'));
 }
+
+
+mongoose.connect(process.env.MONGOLAB_URI);
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.log('communication error', err);
+});
+
+db.once('open', () => {
+  console.log('connected to db');
+});
 
 
 app.get('/', (req, res) => {
---
+++ server/utilities/modifyURL.js
@@ -0,0 +1,12 @@
+// User Story 1:
+// I can pass a URL as a parameter and I will receive a shortened
+//  URL in the JSON response.
+
+// User Story 2:
+// If I pass an invalid URL that doesn't follow the invalid
+//  http://www.example.com
+//  format, the JSON response will contain an error instead.
+
+// User Story 3:
+// When I visit the shortened URL, it will redirect me to
+//  my original link.
