4666f112ea3348912bd2ecaf42c850dd33648ef6
Cody Jewell <cjewell468@gmail.com>
2017-03-17 02:25:52 -0400

    Create an outline for the server-side logic

--- server/utilities/modifyURL.js
+++ server/utilities/modifyURL.js
@@ -12,3 +12,40 @@
 // User Story 3:
 // When I visit the shortened URL, it will redirect me to
 //  my original link.
+
+
+function isValidUrl() {
+  // Take string as a parameter
+  // Check if it is in valid URL format (http and https)
+}
+
+
+// function validURL() {
+//     // If it is valid save the original URL to the db
+// }
+
+
+// function notValidUrl(){
+//     // If it is not valid URL, send back JSON with error.
+// }
+
+
+// function makeShortUrl() {
+//     //  Calculate and create a short URL and save it to that same document
+// }
+
+
+// function createJsonResponse() {
+//     //  Return JSON with the short and original URLs
+// }
+
+
+//    Must be able to navigate to the short URL and be redirected to original
+
+export {
+  isValidUrl,
+  validUrl,
+  notValidUrl,
+  makeShortUrl,
+  createJsonResponse
+}
---
+++ server/utilities/modifyURL.test.js
@@ -0,0 +1,29 @@
+import chai, {
+  expect
+} from 'chai';
+import {
+  isValidUrl,
+  // validUrl,
+  // notValidUrl,
+  // makeShortUrl,
+  // createJsonResponse
+} from './modifyURL';
+
+
+describe('modifyURL tests', () => {
+  xdescribe('isValidUrl tests', () => {
+
+  });
+  // xdescribe('validUrl tests', () => {
+
+  // });
+  // xdescribe('notValidUrl tests', () => {
+
+  // });
+  // xdescribe('makeShortUrl tests', () => {
+
+  // });
+  // xdescribe('createJsonResponse tests', () => {
+
+  // });
+});
